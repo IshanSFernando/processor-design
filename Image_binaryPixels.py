5245cef6decabbb4b8fb79d1dbc65629a606560b
Ishan Fernando <56692754+IshanSFernando@users.noreply.github.com>
2022-06-29 11:05:11 +0530

    Add files via upload
    Generate binary values of pixels and save to a text file

---
+++ Image_binaryPixels.py
@@ -0,0 +1,37 @@
+import numpy as np
+from PIL import Image
+
+
+im = np.array(Image.open('test.jpg').convert('L'))
+print(im.shape)
+col=len(im[0])
+row=len(im)
+if col>256:
+    basewidth = 256
+    img = Image.open('test.jpg').convert('L')
+    wpercent = (basewidth / float(img.size[0]))
+    hsize = int((float(img.size[1]) * float(wpercent)))
+    img = img.resize((basewidth, hsize), Image.ANTIALIAS)
+    im=np.array(img)
+    print(im.shape)
+elif row>256:
+    baseheight = 256
+    img = Image.open('test.jpg').convert('L')
+    hpercent = (baseheight / float(img.size[1]))
+    wsize = int((float(img.size[0]) * float(hpercent)))
+    img = img.resize((wsize, baseheight), Image.ANTIALIAS)
+    im = np.array(img)
+    print(im.shape)
+
+im=np.pad(im,1, mode='constant')
+
+
+im=im.flatten(order='C')
+
+f = open("flat.txt", "w")
+for i in range (len(im)-1):
+    f.write('{0:09b}\n'.format(im[i]))
+f.write('{0:09b}'.format(im[len(im)-1]))
+
+
+f.close()
