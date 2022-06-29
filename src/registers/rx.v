e8e698115381ec024e555c48061ed5b0ebc4432a
NamalSanjayaFernando <namalsanjaya23@gmail.com>
2022-06-29 17:41:18 +0530

    Modify registers to directly output incoming values

--- src/registers/rx.v
+++ src/registers/rx.v
@@ -3,15 +3,12 @@
 module RX (
     input clk,
     input write,
-    input read,
     input [17:0] din,
     output reg[17:0] dout=18'b0
 );
 
-reg [17:0] store=18'b0; // 18-bit register
 always @(posedge clk)
 	begin
-        if (write) store <= din;
-        if (read)  dout  <= store;
+        if (write) dout <= din;
 	end
 endmodule
--- src/registers/tr.v
+++ src/registers/tr.v
@@ -2,16 +2,13 @@ module TR (
     input clk,
     input inc,
     input write,
-    input read,
     input [8:0] din,
     output reg[17:0] dout = 18'b0
 );
 
-reg [8:0] store = 9'b0; // 9-bit register
 always @(posedge clk)
 	begin
-        if (inc) store <= store + 9'b000000001;
-		if (write) store <= din;
-		if (read) dout <= {9'b0,store};
+        if (inc)   dout <= dout + 9'b000000001;
+		if (write) dout <= {9'b0,din};
 	end
 endmodule
