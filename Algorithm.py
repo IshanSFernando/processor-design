b311d1b3062740e0012ce85a0fbd94ac36926ac2
Ishan Fernando <56692754+IshanSFernando@users.noreply.github.com>
2022-06-29 11:02:13 +0530

    Update Algorithm.py

--- Algorithm.py
+++ Algorithm.py
@@ -1,9 +1,31 @@
 import numpy as np
 from PIL import Image
-im = np.array(Image.open('flower.jpg').convert('L'))
+im = np.array(Image.open('test.jpg').convert('L'))
 print(im.shape)
+
 col=len(im[0])
 row=len(im)
+
+if col>256:   ####################change bigger images to 256 size while maintaining the aspect ratio##################
+    basewidth = 256
+    img = Image.open('test.jpg').convert('L')
+    wpercent = (basewidth / float(img.size[0]))
+    hsize = int((float(img.size[1]) * float(wpercent)))
+    img = img.resize((basewidth, hsize), Image.ANTIALIAS)
+    im=np.array(img)
+    print(im.shape)
+elif row>256:
+    baseheight = 256
+    img = Image.open('test.jpg').convert('L')
+    hpercent = (baseheight / float(img.size[1]))
+    wsize = int((float(img.size[0]) * float(hpercent)))
+    img = img.resize((wsize, baseheight), Image.ANTIALIAS)
+    im = np.array(img)
+    print(im.shape)
+
+col=len(im[0])
+row=len(im)
+
 gr_im= Image.fromarray(im).save('first_flower.png')
 ##print(im)
 
@@ -12,6 +34,8 @@ im=np.pad(im,1, mode='constant')
 
 im=im.flatten(order='C')
 ##print(im)
+
+print(len(im))
 
 i=((1+col+1)+1)
 j=1
@@ -44,30 +68,9 @@ while i<((col+2)*(row+2))-(col+2)-1:
 
 ##print(im)
 
-
-####recrearing the downsampled image
-temp=[]
-downsampled=[]
-i=((1+col+1)+1)
-j=1
-while i<((col+2)*(row+2))-(col+2)-1:
-    temp.append(im[i])
-    i=i+2
-    j=j+1
-    if col%2==1:
-        if j>col//2+1:
-            j=1
-            i=i+col+3
-            downsampled.append(temp)
-            temp=[]
-    if col%2==0:
-        if j>col//2:
-            j=1
-            i=i+col+4
-            downsampled.append(temp)
-            temp=[]
-
-##print(downsampled)
-downsampled=np.array(downsampled)
-##print(downsampled.shape)
-gr_im= Image.fromarray(downsampled).save('downsampled_flower.png')
+#################save processed values in a text file###################################
+f = open("processed.txt", "w")
+for i in range (len(im)-1):
+    f.write('{0:09b}\n'.format(im[i]))
+f.write('{0:09b}'.format(im[len(im)-1]))
+f.close()
